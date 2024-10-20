991a039c9d2895b9707c26dace7be1ab1677fe45
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-10-20 20:29:57 +0800

    fix
    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,5 @@
 <script>
 	import { io } from 'socket.io-client';
-	import { spring } from 'svelte/motion';
-
-	let loadingProgress = spring(0, {
-		stiffness: 0.8,
-		damping: 0.8
-	});
-
 	import { onMount, tick, setContext } from 'svelte';
 	import {
 		config,
@@ -148,14 +141,7 @@
 		// 更新进度条
 		const progressBar = document.getElementById('progress-bar');
 		if (progressBar) {
-			loadingProgress.subscribe((value) => {
-				requestAnimationFrame(() => {
-					if (progressBar) {
-						progressBar.style.width = `${Math.min(value, 100)}%`;
-					}
-				});
-			});
-			await loadingProgress.set(100);
+			progressBar.style.width = '100%';
 		}
 
 		// 移除启动画面
