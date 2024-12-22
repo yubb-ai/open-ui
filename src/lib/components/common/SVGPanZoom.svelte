efd88521eb4f5f6b5a0876ac6cc772367454debb
Clivia <132346501+Yanyutin753@users.noreply.github.com>
2024-12-23 00:26:27 +0800

    feat(SVGPanZoom): 添加下载按钮并优化下载文件命名
    - 在 SVGPanZoom 组件中添加下载按钮
    - 优化下载文件命名，使用当前日期时间戳
    - 调整下载按钮位置，使其更醒目

--- src/lib/components/common/SVGPanZoom.svelte
+++ src/lib/components/common/SVGPanZoom.svelte
@@ -22,30 +22,25 @@
 		html2canvas(sceneElement).then((canvas) => {
 			const link = document.createElement('a');
 			link.href = canvas.toDataURL('image/png');
-			link.download = 'mermaid_diagram.png';
+			link.download = `mermaid_${new Date().toISOString().replace(/[:.]/g, '-')}.png`;
 			link.click();
 		});
 	}
 </script>
 
 <div bind:this={sceneParentElement} class={className}>
+	<button class="download-btn" on:click={downloadSVGAsPNG}>
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-6 h-6">
+			<path
+				d="M10.75 2.75a.75.75 0 0 0-1.5 0v8.614L6.295 8.235a.75.75 0 1 0-1.09 1.03l4.25 4.5a.75.75 0 0 0 1.09 0l4.25-4.5a.75.75 0 0 0-1.09-1.03l-2.955 3.129V2.75Z"
+			></path>
+			<path
+				d="M3.5 12.75a.75.75 0 0 0-1.5 0v2.5A2.75 2.75 0 0 0 4.75 18h10.5A2.75 2.75 0 0 0 18 15.25v-2.5a.75.75 0 0 0-1.5 0v2.5c0 .69-.56 1.25-1.25 1.25H4.75c-.69 0-1.25-.56-1.25-1.25v-2.5Z"
+			></path>
+		</svg>
+	</button>
 	<div bind:this={sceneElement} class="relative flex h-full max-h-full justify-center items-center">
 		{@html svg}
-		<button class="download-btn" on:click={downloadSVGAsPNG}>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 20 20"
-				fill="currentColor"
-				class="w-6 h-6"
-			>
-				<path
-					d="M10.75 2.75a.75.75 0 0 0-1.5 0v8.614L6.295 8.235a.75.75 0 1 0-1.09 1.03l4.25 4.5a.75.75 0 0 0 1.09 0l4.25-4.5a.75.75 0 0 0-1.09-1.03l-2.955 3.129V2.75Z"
-				></path>
-				<path
-					d="M3.5 12.75a.75.75 0 0 0-1.5 0v2.5A2.75 2.75 0 0 0 4.75 18h10.5A2.75 2.75 0 0 0 18 15.25v-2.5a.75.75 0 0 0-1.5 0v2.5c0 .69-.56 1.25-1.25 1.25H4.75c-.69 0-1.25-.56-1.25-1.25v-2.5Z"
-				></path>
-			</svg>
-		</button>
 	</div>
 </div>
 
